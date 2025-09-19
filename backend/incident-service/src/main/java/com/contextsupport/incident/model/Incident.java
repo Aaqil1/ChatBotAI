d286d26514d5c742aee3d4e2b614efa0fe04c65e
Ghori Aaqil <ghoriaaqil007@gmail.com>
2025-09-19 21:46:18 +0530

    Add constructors for Mongo document models

--- backend/incident-service/src/main/java/com/contextsupport/incident/model/Incident.java
+++ backend/incident-service/src/main/java/com/contextsupport/incident/model/Incident.java
@@ -4,14 +4,20 @@ import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Singular;
 
 @Data
 @Builder
+@AllArgsConstructor(onConstructor = @__(@PersistenceConstructor))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Document(collection = "incidents")
 public class Incident {
 
